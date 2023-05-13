493f5706c11e4251b215bede593798d42a401446
Harole <teraos@proton.me>
2023-05-14 00:20:25 +0700

    Uploading CrossOver-made Wine precompiled binary

---
+++ precompiled/cx-21.2.0/include/distversion.h
@@ -0,0 +1,2 @@
+#define WINDEBUG_WHAT_HAPPENED_MESSAGE "This can be caused by a problem in the program or a deficiency in Wine."
+#define WINDEBUG_USER_SUGGESTION_MESSAGE "If this problem is not present under Windows and has not been reported yet, you can save the detailed information to a file using the \"Save As\" button, then file a bug report and attach that file to the report."
